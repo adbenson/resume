613afebf7d3e2204cfe3fa96dd2042f2bafc6a06
unknown <abenson@LT-CNU350BHXS.c.systemsinmotion.com>
2015-01-08 16:16:56 -0500

    New Address! And I like it without the dot now.

--- content/header.js
+++ content/header.js
@@ -1,14 +1,14 @@
 header_content = 
 {
-	"name":	"Andrew D. Benson",
+	"name":	"Andrew D Benson",
 	"credentials": "",
 	"contact": 
 	{
 		"email": "drew@adbenson.net",
 		"phone": "(734)709-5496",
-		"address": "343 Edison Ave",
+		"address": "8474 Barrington Dr",
 		"address2": "",
-		"location": "Ypsilanti, MI 48197"
+		"location": "Ypsilanti, MI 48198"
 	},
 	"title": "Creative, skilled, and driven software developer"
 };
